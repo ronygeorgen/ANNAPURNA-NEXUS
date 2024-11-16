3f40eedd789f36dcf285bf11b4c36bac50fc1114
Rony George <ronygeorgengit@gmail.com>
2024-11-16 07:50:52 +0530

    axios error solved

--- Backend/api-gateway/gateway_service/gateway_service/settings.py
+++ Backend/api-gateway/gateway_service/gateway_service/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'gateway_app.jwt_middleware.jwt_middleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'gateway_app.jwt_middleware.jwt_middleware',
 ]
 
 ROOT_URLCONF = 'gateway_service.urls'
